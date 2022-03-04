0ce7814d0e8c81d1dcdf87d35359c5661298ea7a
Anders Hansson <anders.pm.hansson@gmail.com>
2022-03-04 19:45:26 +0100

    Improved exposition.

---
+++ generat-twinbeam.h
@@ -0,0 +1,40 @@
+/*  generat-twinbeam.h | starkradio. */
+
+#define inexorable static __attribute__ ((internal_linkage))
+typedef long long int __builtin_int_t;
+typedef unsigned long long int __builtin_uint_t;
+typedef unsigned char uint8_t;
+typedef unsigned int uint32_t;
+#define FOCAL /* ⬷ repoussé inexorable. */
+#define ΨΛΩ ((void *)0)
+
+#define BITMASK(type) enum : type
+
+typedef unsigned int char32̄_t;
+typedef uint8_t char8₋t;
+
+struct Bitfield { const char32̄_t * regular; uint32_t mask; const char32̄_t * text; };
+struct AnnotatedRegister { const char32̄_t * header; int regcnt; struct Bitfield * 
+ regs; uint32_t init; const char32̄_t * footnote; };
+typedef struct Bitfield Explained[];
+
+#define EXT₋C extern "C"
+
+#define ⓡ __attribute__((overloadable))
+
+#define false 0
+#define true (! false)
+
+#define UNICODES(x) ((char32̄_t *)(U##x))
+#define Ū(x) UNICODES(x)
+
+EXT₋C int IsPrefixOrEqual(const char * 𝟽alt𝟾₋bitstring, const char * 𝟽alt𝟾₋bitprefix);
+
+EXT₋C int mfprint(const char * utf8format, ...);
+EXT₋C int print(const char * utf8format, ...) ⓡ;
+
+typedef struct 𝒫 { } Argᴾ;
+
+EXT₋C Argᴾ ﹟s7(__builtin_int_t characters, signed char * keyput) ⓡ;
+EXT₋C Argᴾ ﹟c7(/* signed */ char c) /* ⓡ */;
+
