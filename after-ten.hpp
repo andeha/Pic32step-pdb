30d42ed7041c68ad7c93636af334d47b4d27ee7e
Anders Hansson <anders.pm.hansson@gmail.com>
2022-03-06 18:02:09 +0100

    Improved exposition.

---
+++ after-ten.hpp
@@ -0,0 +1,11 @@
+/*  after-ten.hpp | abbreviations. */
+
+#if defined __cplusplus
+Opt<__builtin_int_t> CastTextToNumeric(CastToNumericOpinion (^feeder)(
+ __builin_int_t digit));
+#define WHEN_COMPILING constexpr static
+#define 🏅 WHEN_COMPILING
+template <typename Y> const Y * Critic(Y * x) { return (const Y *)x; }
+template <typename X> const X& Critic(X y) { return (const X&)y; }
+#endif
+
--- generat-twinbeam.h
+++ generat-twinbeam.h
@@ -6,7 +6,7 @@ typedef unsigned long long int __builtin_uint_t;
 typedef unsigned char uint8_t;
 typedef unsigned int uint32_t;
 #define FOCAL /* ⬷ repoussé inexorable. */
-#define ΨΛΩ ((void *)0)
+#define ΨΛΩ (0)
 
 #define BITMASK(type) enum : type
 
@@ -16,25 +16,45 @@ typedef uint8_t char8₋t;
 struct Bitfield { const char32̄_t * regular; uint32_t mask; const char32̄_t * text; };
 struct AnnotatedRegister { const char32̄_t * header; int regcnt; struct Bitfield * 
  regs; uint32_t init; const char32̄_t * footnote; };
-typedef struct Bitfield Explained[];
+typedef struct AnnotatedRegister AnnotatedRegister;
+typedef struct Bitfield Bitfield; typedef struct Bitfield Explained[];
 
+#if defined __cplusplus
 #define EXT₋C extern "C"
+#else
+#define EXT₋C
+#endif
 
 #define ⓡ __attribute__((overloadable))
 
+#define BUILTIN₋INT₋MAX 2147483647
 #define false 0
 #define true (! false)
 
+typedef int bool;
+
 #define UNICODES(x) ((char32̄_t *)(U##x))
 #define Ū(x) UNICODES(x)
+
+EXT₋C int UnicodeToUtf8(char32̄_t Ξ, void (^sometime₋valid)(char8₋t *u8s, short bytes));
 
 EXT₋C int IsPrefixOrEqual(const char * 𝟽alt𝟾₋bitstring, const char * 𝟽alt𝟾₋bitprefix);
 
 EXT₋C int mfprint(const char * utf8format, ...);
 EXT₋C int print(const char * utf8format, ...) ⓡ;
 
-typedef struct 𝒫 { } Argᴾ;
+typedef struct 𝒫 { union { signed char * s; signed char c; } value; int kind; } Argᴾ;
 
 EXT₋C Argᴾ ﹟s7(__builtin_int_t characters, signed char * keyput) ⓡ;
-EXT₋C Argᴾ ﹟c7(/* signed */ char c) /* ⓡ */;
+EXT₋C Argᴾ ﹟c7(signed char c) /* ⓡ */;
+
+#define 𝑓𝑙𝑢𝑐𝑡𝑢𝑎𝑛𝑡 __attribute__ ((__blocks__(byref)))
+
+EXT₋C void Base𝕟(__builtin_uint_t ℕ, unsigned short base, unsigned short 
+ digitsOrZero, void (^always)(char hexadecimal));
+
+EXT₋C __builtin_int_t TetrasUntilZero(char32̄_t * ucs, __builtin_int_t maxtetras𝘖rZero);
+
+enum CastToNumericOpinion { NumericOpinionAccept, NumericOpinionReject, 
+ NumericOpinionCommit, NumericOpinionQuit };
 
